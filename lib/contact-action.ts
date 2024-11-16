34c28460cb1109edc0c65317a3ec9813404dfaf2
knextkoder <knextkoderklan@gmail.com>
2024-11-16 16:56:31 +0100

    Add Resend integration for email functionality and update .gitignore

--- lib/contact-action.ts
+++ lib/contact-action.ts
@@ -1,4 +1,6 @@
 'use server'
+import { Resend } from 'resend';
+
 interface formData{
     company: string;
     name: string;
@@ -12,6 +14,17 @@ export const sendMessage = async (data:formData) => {
     }else if (data.message.length < 1 && data.company.length < 1 && data.email.length < 1 && data.name.length < 1){
         console.log("Empty data")
     }else{
-        console.log(data)
+        const resend = new Resend(`${process.env.RESEND_API_KEY}`);
+        
+        resend.emails.send({
+          from: 'onboarding@resend.dev',
+          to: 'info@nikentoladgroup.com',
+          subject: `Message from ${data.name} at ${data.company}, ${data.email}`,
+          html: `<p>${data.message}</p>`
+        });
+
+        console.log("Message sent")
     }
 }
+
+
